5eb26d578c0285169bfd80add9e680a47bb5f3fa
MeeganM <mositoameegan@gmail.com>
2022-07-09 12:23:32 -0700

    the main file

--- main.h
+++ main.h
@@ -2,7 +2,28 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ * struct vtype - struct vtype
+ * @tp: tp
+ * @f: function
+ *
+ */
 
+typedef struct vtype
+{
+	char tp;
+	void (*f)();
+}vtype_t;
 int _printf(const char *format, ...);
+void print_char(va_list valist);
+void print_int(va_list valist);
+void print_float(va_list valist);
+void print_string(va_list valist);
+void _write_buffer(char *buffer, int *index);
+int _strlen(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void format_s(va_list valist, char *buffer, int *index);
+void format_c(va_list valist, char *buffer, int *index);
+void format_d(va_list valist, char *buffer, int *index);
 
 #endif
